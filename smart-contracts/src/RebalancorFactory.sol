c04e38318d1309bcb9e347746a6b3a0d917ddff6
Petrovska <petrovska@badger.com>
2024-05-04 01:10:04 +1000

    feat: add counter tracking per eoa

--- smart-contracts/src/RebalancorFactory.sol
+++ smart-contracts/src/RebalancorFactory.sol
@@ -11,10 +11,13 @@ contract Factory {
     // eoa -> smart pools
     mapping(address => address[]) public smartPools;
 
+    mapping(address => uint256) public smartPoolCount;
+
     /// @param _poolName The name of the pool
     function createRebalancor(string memory _poolName) external returns (address) {
         address rebalancor = address(new Rebalancor(msg.sender, _poolName));
         smartPools[msg.sender].push(rebalancor);
+        smartPoolCount[msg.sender] += 1;
         return rebalancor;
     }
 }
